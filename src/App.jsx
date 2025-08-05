63da201245afe8bcbe269e0340788811e58dae83
DanielMachado-web <danielmachadojhs@gmail.com>
2025-08-05 08:10:52 -0300

    alteração

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+
+import './index.css'
+
+function App() {
+
+  const [list, setList] = useState([{ id: uuid(), task: 'DANIEL MACHADO' }]);
+  const [task, setTask] = ('');
+
+
+  // "comprar abacate", "levar  o Nico para passear", "daniel ribeiro machado", "comprar abacate", "levar  o Nico para passear", "daniel ribeiro machado"
+  function inputMudou(event) {
+
+    setTask(event.target.value)
+    //console.log(event.target.value)
+    //setList([{ id: uuid(), task: event.target.value }]);
+    
+
+  }
+
+  function cliqueiNoBotao() {
+    setList([{ id: uuid(), task }]);
+  }
+  return (
+    <div>
+      <input className='input' onChange={inputMudou} placeholder="oque tenha que fazer com o projeto"></input>
+      <button className='button' onClick={cliqueiNoBotao}>Adicionar</button>
+
+      <u>
+        {
+          list.map(item => (
+            <li key={item.id}>{item.task}</li>
+          ))
+
+        }
+      </u>
+
+    </div>
+  )
+}
+
+
+export default App

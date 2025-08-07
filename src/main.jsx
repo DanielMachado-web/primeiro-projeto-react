995cfdbeb7d3e04612bed4957cb3754c6c91f3bb
DanielMachado-web <danielmachadojhs@gmail.com>
2025-08-07 00:38:30 -0300

    alteração

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import GlobalStyle from './globalStyles.js'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyle />
     <App />

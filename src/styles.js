5d3e555f6d6fdc2467672d1058634365a5f7b67e
DanielMachado-web <danielmachadojhs@gmail.com>
2025-08-09 22:57:15 -0300

    alteracao

--- src/styles.js
+++ src/styles.js
@@ -4,4 +4,9 @@ import styled from "styled-components";
 export const Container = styled.div`
 
     background-color: linear-gradient(90deg, #383838 0%, #000000, 81.25%);
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `

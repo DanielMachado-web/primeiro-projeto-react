1ca55b11941237d8a6e399b566b021350fd88155
DanielMachado-web <danielmachadojhs@gmail.com>
2025-08-11 10:43:46 -0300

    alteração

--- src/styles.js
+++ src/styles.js
@@ -25,7 +25,8 @@ export const Input = styled.Input`
     border: 2px solid #000;
     padding: 30px 20px;
     border-radius: 10px;
-    
+    width: 340px;
+
 `
 export const Button = styled.Button`
     background: #8052ec;
@@ -38,6 +39,25 @@ export const Button = styled.Button`
     line-height: 1.5;
     height: 40px;
     font-weight: 400;
+    width: 130px;
     
 
+`
+export const ListItem = styled.div`
+    background: #E4E4E4;
+    box-shadow: 1px 4px 10px rgba(0,0,0,0.2);
+    border-radius: 5px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 30px;
+    padding: 0, 10px;
+    width: 500px;
+
+
+    li{
+        list-style: none;
+    }
+    
 `

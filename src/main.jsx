da0a7ff2ca1174d54a9c3af2f1fab9fa89f4db57
DanielMachado-web <danielmachadojhs@gmail.com>
2025-08-13 09:48:53 -0300

    alteraçao

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './main.jsx'
+import { React } from 'react'
+import { ReactDOM } from 'react-dom/client'
 import App from './App.jsx'
 import GlobalStyle from './globalStyles.js'
 
